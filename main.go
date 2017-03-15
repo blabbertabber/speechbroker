5359edb23ee69cb3e26588bf7ee38246611513e8
Brian Cunnie <brian.cunnie@gmail.com>
2017-03-15 10:37:38 -0700

    Return a UUID after the POST
    Signed-off-by: Brendan E Cunnie <saintbrendan@gmail.com>

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
+// curl -X POST -d "1234" http://diarizer.blabbertabber.com:8080/api/v1/upload
+
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/satori/go.uuid"
 )
 
+const PORT=8080
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path)
+	conversationUUID := uuid.NewV4() // TODO(brian) handle error
+	w.Write([]byte(conversationUUID.String()))
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/api/v1/upload", handler)
+	http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
 }
