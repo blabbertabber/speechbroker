5572a573ceac9be302cc084ed8c1cef4ce676bda
Brendan Cunnie <saintbrendan@gmail.com>
2017-03-22 20:10:12 -0700

    Upload server returns results URL
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>

--- main.go
+++ main.go
@@ -12,16 +12,19 @@ import (
 	"io/ioutil"
 )
 
-const PORT=8080
+const PORT = 8080
+
 var dataRootDir = filepath.FromSlash("/opt/blabbertabber")
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	conversationUUID := uuid.NewV4()
 	dataDir := filepath.Join(dataRootDir, conversationUUID.String())
-	_ = os.MkdirAll(dataDir, 0777) // TODO(brian) handle error
-	bytes, _ := ioutil.ReadAll(r.Body) // TODO(brian) handle error
+	_ = os.MkdirAll(dataDir, 0777)                                           // TODO(brian) handle error
+	bytes, _ := ioutil.ReadAll(r.Body)                                       // TODO(brian) handle error
 	_ = ioutil.WriteFile(filepath.Join(dataDir, "meeting.wav"), bytes, 0644) // TODO(brian) handle error
-	w.Write([]byte(conversationUUID.String()))
+	// return weblink to client "http://diarizer.blabbertabber.com:8080/results/UUID"
+	w.Write([]byte(fmt.Sprint("http://diarizer.blabbertabber.com:8080/results/", conversationUUID.String())))
+	// kick off diarization in the background
 }
 
 func main() {
