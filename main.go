e51ce4a3de1d001fc4dd4499dcf90dfe5bd6dced
Brian Cunnie <brian.cunnie@gmail.com>
2017-03-29 16:01:29 -0700

    Enables full upload of file
    - previously uploaded only 984422 bytes
    - uses multipart POST
    - diarization does not work, yet

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
-// curl -X POST -d "1234" http://diarizer.blabbertabber.com:8080/api/v1/upload
+// curl -X POST -F "a=1234" https://diarizer.blabbertabber.com:9443/api/v1/upload
+// curl -X POST -F "meeting.wav=@/Users/cunnie/Google Drive/BlabberTabber/ICSI-diarizer-sample-meeting.wav" https://diarizer.blabbertabber.com:9443/api/v1/upload
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/satori/go.uuid"
-	"os"
-	"path/filepath"
-	"io/ioutil"
-	"crypto/tls"
-	"time"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 const PORT = 9443
@@ -23,52 +22,53 @@ var certPath = filepath.FromSlash("/etc/pki/nginx/diarizer.blabbertabber.com.crt
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	conversationUUID := uuid.NewV4()
-	dataDir := filepath.Join(dataRootDir, conversationUUID.String())
-	_ = os.MkdirAll(dataDir, 0777)                                           // TODO(brian) handle error
-	bytes, _ := ioutil.ReadAll(r.Body)                                       // TODO(brian) handle error
-	_ = ioutil.WriteFile(filepath.Join(dataDir, "meeting.wav"), bytes, 0644) // TODO(brian) handle error
-	// return weblink to client "http://diarizer.blabbertabber.com:8080/results/UUID"
-	w.Write([]byte(fmt.Sprint("https://diarizer.blabbertabber.com/", conversationUUID.String())))
+	uuid := conversationUUID.String()
+	dataDir := filepath.Join(dataRootDir, uuid)
+	err := os.MkdirAll(dataDir, 0777)
+	if err != nil {
+		log.Fatal("MkdirAll: ", err)
+	}
+	// bytes, err := ioutil.ReadAll(r.Body)
+	reader, err := r.MultipartReader()
+	if err != nil {
+		log.Fatal("MultipartReader: ", err)
+	}
+	for {
+		part, err := reader.NextPart()
+		if err == io.EOF {
+			break
+		}
+		// dst, err := os.Create("/home/sanat/" + part.FileName())
+		dst, err := os.Create(filepath.Join(dataDir, "meeting.wav"))
+		if err != nil {
+			log.Fatal("Create: ", err)
+		}
+		defer dst.Close()
+
+		if _, err := io.Copy(dst, part); err != nil {
+			log.Fatal("Copy: ", err)
+			// http.Error(w, err.Error(), http.StatusInternalServerError)
+			// return
+		}
+	}
+	// return weblink to client "https://diarizer.blabbertabber.com/UUID"
+	w.Write([]byte(fmt.Sprint("https://diarizer.blabbertabber.com/", uuid)))
 	// kick off diarization in the background
+	diarizationCommand := exec.Command("docker",
+		"run",
+		"--volume=/var/blabbertabber:/blabbertabber",
+		"--workdir=/speaker-diarization",
+		"blabbertabber/aalto-speech-diarizer",
+		"/speaker-diarization/spk-diarization2.py",
+		fmt.Sprintf("/blabbertabber/UploadServer/%s/meeting.wav", uuid),
+		"-o", fmt.Sprintf("/blabbertabber/diarizer/%s/results.txt", uuid))
+	err = diarizationCommand.Run()
+	if err != nil {
+		log.Fatal("Run: ", err)
+	}
 }
 
 func main() {
-	//&tls.Config{
-	//	// Causes servers to use Go's default ciphersuite preferences,
-	//	// which are tuned to avoid attacks. Does nothing on clients.
-	//	PreferServerCipherSuites: true,
-	//	// Only use curves which have assembly implementations
-	//	CurvePreferences: []tls.CurveID{
-	//		tls.CurveP256,
-	//		tls.X25519, // Go 1.8 only
-	//	},
-	//	// If you can take the compatibility loss of the Modern configuration, you should then also set MinVersion and CipherSuites.
-	//	MinVersion: tls.VersionTLS12,
-	//	CipherSuites: []uint16{
-	//		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-	//		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-	//		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
-	//		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
-	//		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-	//		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-	//
-	//		// Best disabled, as they don't provide Forward Secrecy,
-	//		// but might be necessary for some clients
-	//		// tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-	//		// tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-	//	},
-	//}
-
-	//srv := &http.Server{
-	//	ReadTimeout:  5 * time.Second,
-	//	WriteTimeout: 5 * time.Second,
-	//	Handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-	//		w.Header().Set("Connection", "close")
-	//		url := "https://" + req.Host + req.URL.String()
-	//		http.Redirect(w, req, url, http.StatusMovedPermanently)
-	//	}),
-	//}
-
 	http.HandleFunc("/api/v1/upload", handler)
 	err := http.ListenAndServeTLS(fmt.Sprintf(":%d", PORT), certPath, keyPath, nil)
 	if err != nil {
