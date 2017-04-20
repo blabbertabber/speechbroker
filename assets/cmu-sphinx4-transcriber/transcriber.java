029834df2da2c35661bbd7da66427cf48342b0f7
Brian Cunnie <brian.cunnie@gmail.com>
2017-04-19 18:46:19 -0700

    Enable transcription via java front-end program

---
+++ assets/cmu-sphinx4-transcriber/transcriber.java
@@ -0,0 +1,37 @@
+// copied from CMU Sphinx Samples
+// We don't need no package 'cause we're bad boys
+// package com.example;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+
+public class Transcriber {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration();
+
+        configuration
+                .setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration
+                .setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration
+                .setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(
+                configuration);
+        InputStream stream = new FileInputStream(new File("meeting.wav"));
+
+        recognizer.startRecognition(stream);
+        SpeechResult result;
+        while ((result = recognizer.getResult()) != null) {
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+        }
+        recognizer.stopRecognition();
+    }
+}
