6d166f0eac77afb561fe75d9ab73c70cad008ceb
Brian Cunnie <brian.cunnie@gmail.com>
2017-04-23 17:54:41 -0700

    WIP:  HTML + JS launchpad
    - javascript parsets '?meeting=guid' to strip out guid

--- assets/charlotte.js
+++ assets/charlotte.js
@@ -1,11 +1,11 @@
 meetingGuid = window.location.href.split('?')[1];
+meetingGuid = meetingGuid.split('=')[1];
 meetingURL = 'https://diarizer.blabbertabber.com/' + meetingGuid + '/diarization.txt';
 
 jQuery('#xyz').html("jQuery works.  But we haven't downloaded the file yet");
 
 jQuery.get(meetingURL, function(data) {
-  alert("We are within the get function.")
-  jQuery('#xyz').html("we hit the get file function!!!!");
+  jQuery('#xyz').html(data.replace("\n", "<br />"));
 });
 
-jQuery('#last').html("really done" + new Date());
+jQuery('#asdf').html("really done"); // + new Date());
