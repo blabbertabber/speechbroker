806044b3b4939f28ba481439b08c144f23dc425f
Brian Cunnie <brian.cunnie@gmail.com>
2017-05-31 05:16:03 +0800

    Parse IBM Service Credentials from JSON
    - will be a flag passed to the Diarizer Server that points
      to the JSON file with credentials, e.g.
      `./DiarizerServer -IBMCredFile /var/blabbertabber/ibm.json`
    - This lays the groundwork for the DiarizerServer to pass
      the IBM Credentials to the Docker container which will
      upload the meeting.wav file to IBM & download the JSON results

---
+++ ibmservicecreds/ibmservicecreds_suite_test.go
@@ -0,0 +1,13 @@
+package ibmservicecreds_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestIbmservicecreds(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Ibmservicecreds Suite")
+}
---
+++ ibmservicecreds/ibmservicecreds_test.go
@@ -0,0 +1,61 @@
+package ibmservicecreds_test
+
+import (
+	. "github.com/blabbertabber/DiarizerServer/ibmservicecreds"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"strings"
+)
+
+var _ = Describe("IBMServiceCreds", func() {
+	Context(".ReadCredsFromReader", func() {
+		Context("When a reader is passed an empty JSON", func() {
+			It("returns an empty struct", func() {
+				sourceReader := strings.NewReader("{}")
+				expectation := IBMServiceCreds{}
+				readCreds, err := ReadCredsFromReader(sourceReader)
+				Expect(err).To(BeNil())
+				Expect(readCreds).To(Equal(expectation))
+			})
+		})
+		Context("When a reader is passed a populated JSON", func() {
+			It("returns a populated struct", func() {
+				source, err := ioutil.ReadFile("../assets/test/ibm_service_creds.json")
+				Expect(err).To(BeNil())
+				sourceReader := strings.NewReader(string(source))
+				expectation := IBMServiceCreds{
+					Url:      "https://stream.watsonplatform.net/speech-to-text/api",
+					Username: "9f6cdead-d9d3-49db-96e4-deadbeefdead",
+					Password: "8rgJeCunnie8",
+				}
+				readCreds, err := ReadCredsFromReader(sourceReader)
+				Expect(err).To(BeNil())
+				Expect(readCreds).To(Equal(expectation))
+			})
+		})
+	})
+	Context(".ReadCredsFromPath", func() {
+		Context("When path is exists and is valid JSON", func() {
+			It("returns the expected struct", func() {
+				readCreds, err := ReadCredsFromPath("../assets/test/ibm_service_creds.json")
+				Expect(err).To(BeNil())
+				expectation := IBMServiceCreds{
+					Url:      "https://stream.watsonplatform.net/speech-to-text/api",
+					Username: "9f6cdead-d9d3-49db-96e4-deadbeefdead",
+					Password: "8rgJeCunnie8",
+				}
+				Expect(readCreds).To(Equal(expectation))
+			})
+		})
+		Context("When path is non-existent", func() {
+			It("panics", func() {
+				Expect(func() {
+					ReadCredsFromPath("/non/existent/path")
+				}).Should(Panic())
+			})
+		})
+	})
+
+})
