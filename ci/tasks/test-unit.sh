923ff282ca0fbf749b2e7739789521a61fe57887
Brian Cunnie <brian.cunnie@gmail.com>
2017-06-29 05:10:55 -0700

    Download dependencies before running Ginkgo tests
    fixes:
    ```
    httphandler/httphandler.go:5:2: cannot find package
    "github.com/google/uuid" in any of:
    /usr/local/go/src/github.com/google/uuid (from $GOROOT)
        /go/src/github.com/google/uuid (from $GOPATH)
    ```

--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -3,4 +3,5 @@
 mkdir -p $GOPATH/src/github.com/blabbertabber/
 cp -Rp src/ /go/src/github.com/blabbertabber/speechbroker
 cd $GOPATH/src/github.com/blabbertabber/speechbroker
+go get ./...
 ginkgo -v -r .
