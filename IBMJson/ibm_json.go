c9bfe1d801adc66890a5c34a237d1f324efe8eee
Brian Cunnie <brian.cunnie@gmail.com>
2017-07-20 07:56:18 -0700

    `ibm_json` accepts `-in` and `-out` flags
    - useful when calling via `exec.Command()`: can specify the inputs
      and outputs directly rather than invoking a `bash` shell
      with redirects (i.e. `<` and `>`)
    - added a missing error-check

--- IBMJson/ibm_json.go
+++ IBMJson/ibm_json.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/blabbertabber/speechbroker/IBMJson/emitblabbertabber"
 	"github.com/blabbertabber/speechbroker/IBMJson/parseibm"
@@ -12,10 +13,32 @@ import (
 
 func main() {
 	// source := []byte(`"result_index": 0, "results": [], "speaker_labels": []`)
-	source, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		panic(err.Error())
+	var inFile = flag.String("in", "",
+		"path to JSON input file created by IBM's Speech-to-Text, e.g. '/blabbertabber/diarizationResults/0.json.txt'")
+	var outFile = flag.String("out", "",
+		"path to output file, e.g. 'summary.json'")
+	flag.Parse()
+
+	var source []byte
+	var err error
+
+	switch *inFile {
+	case "":
+		source, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err.Error())
+		}
+	default:
+		inHandle, err := os.Open(*inFile)
+		if err != nil {
+			panic(err.Error())
+		}
+		source, err = ioutil.ReadAll(inHandle)
+		if err != nil {
+			panic(err.Error())
+		}
 	}
+
 	var input parseibm.IBMTranscription // the whole, complete transcription
 	err = json.Unmarshal(source, &input)
 	if err != nil {
@@ -26,6 +49,17 @@ func main() {
 		panic(err.Error())
 	}
 	bytes, err := json.Marshal(transcriptions)
+	if err != nil {
+		panic(err.Error())
+	}
 
-	fmt.Println(string(bytes))
+	switch *outFile {
+	case "":
+		fmt.Println(string(bytes))
+	default:
+		err := ioutil.WriteFile(*outFile, bytes, 0644)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
 }
