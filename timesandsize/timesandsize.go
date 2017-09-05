173750863dc44205285e85958e3e9cd2a7c8b397
Brian Cunnie <brian.cunnie@gmail.com>
2017-09-04 21:06:26 -0700

    Times are output'ed in ISO 8061-ish format
    - javascript _should_ be able to parse those dates
      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
    - tests have a 1-second difference between transcription time & diarization time to
      avoid accidental swapping

--- timesandsize/timesandsize.go
+++ timesandsize/timesandsize.go
@@ -4,16 +4,25 @@ package timesandsize
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
 
 type TimesAndSize struct {
-	WaveFileSizeInByte               int       `json:"wav_file_size_in_bytes"`
-	Diarizer                         string    `json:"diarizer"`
-	Transcriber                      string    `json:"transcriber"`
-	EstimatedTranscriptionFinishTime time.Time `json:"estimated_transcription_finish_time"`
-	EstimatedDiarizationFinishTime   time.Time `json:"estimated_diarization_finish_time"`
+	WaveFileSizeInByte               int      `json:"wav_file_size_in_bytes"`
+	Diarizer                         string   `json:"diarizer"`
+	Transcriber                      string   `json:"transcriber"`
+	EstimatedDiarizationFinishTime   JSONTime `json:"estimated_diarization_finish_time"`
+	EstimatedTranscriptionFinishTime JSONTime `json:"estimated_transcription_finish_time"`
+}
+
+type JSONTime time.Time
+
+func (t JSONTime) MarshalJSON() ([]byte, error) {
+	//do your serializing here
+	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("2006-01-02T15:04:05-0700"))
+	return []byte(stamp), nil
 }
 
 func (tas *TimesAndSize) WriteTimesAndSizeToWriter(w io.Writer) {
