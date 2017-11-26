49d28256179e5fa8a5a21c6614233eae7ea72d91
Brendan Cunnie <saintbrendan@gmail.com>
2017-11-25 17:08:24 -0800

    Concourse job tests JS front-end
    - adds new job, `test-front-end`
    - adds new resource, `diarizer_results`, which is the `DiarizerResults`
      git repo.
    - rename Concourse resource `src` to `speech_broker`; `src` was okay
      when there was only one source repo, but too generic when there are
      two.
    
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>

--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 mkdir -p $GOPATH/src/github.com/blabbertabber/
-cp -Rp src/ /go/src/github.com/blabbertabber/speechbroker
+cp -Rp speech_broker/ /go/src/github.com/blabbertabber/speechbroker
 cd $GOPATH/src/github.com/blabbertabber/speechbroker
 go get ./...
 ginkgo -v -r .
